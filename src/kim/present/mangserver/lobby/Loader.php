ed924b9ebb98dc88bc7bdc11d0662b1c4cb6d915
PresentKim <debe3721@gmail.com>
2020-10-16 10:35:14 +0900

    Init plugin base

---
+++ src/kim/present/mangserver/lobby/Loader.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ *
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * @author  PresentKim
+ * @link    https://github.com/PresentKim
+ * @license https://www.gnu.org/licenses/mit MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace kim\present\mangserver\lobby;
+
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase{
+    public function onLoad() : void{
+        LobbyManager::getInstance();
+    }
+}
---
+++ src/kim/present/mangserver/lobby/LobbyManager.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ *
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * @author  PresentKim
+ * @link    https://github.com/PresentKim
+ * @license https://www.gnu.org/licenses/mit MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace kim\present\mangserver\lobby;
+
+use pocketmine\Server;
+use pocketmine\utils\SingletonTrait;
+
+class LobbyManager{
+    use SingletonTrait;
+
+    public function __construct(){
+        $this->init();
+    }
+
+    public function init(){
+    }
+}
